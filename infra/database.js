e8f32c3d43c0d70afcaa8a56d3b59b7617a6f57d
Miniko <xavierdeoliveiramauricio@gmail.com>
2025-01-11 01:11:19 -0300

    feat: add database connection

---
+++ infra/database.js
@@ -0,0 +1,45 @@
+import { Client } from "pg";
+
+async function getNewClient() {
+  const pgClient = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+  });
+
+  pgClient.connect();
+  return pgClient;
+}
+
+async function query(query, params = []) {
+  let client;
+
+  try {
+    client = await getNewClient();
+    const result = await client.query(query, params);
+    return result;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  } finally {
+    await client.end();
+  }
+}
+
+function getSSL() {
+  if (process.env.POSTGRES_CA)
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+
+  return process.env.NODE_ENV === "production";
+}
+
+const database = {
+  getNewClient,
+  query,
+};
+
+export default database;

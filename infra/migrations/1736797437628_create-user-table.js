ae5deb3106233033da21ebb7e3ef17420637fabc
Maurício Xavier Oliveira <119255200+mini-niko@users.noreply.github.com>
2025-01-13 19:56:43 +0000

    chore: add `create-table-user` migration

---
+++ infra/migrations/1736797437628_create-user-table.js
@@ -0,0 +1,71 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable camelcase */
+
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable("users", {
+    id: {
+      type: "uuid",
+      default: pgm.func("gen_random_uuid()"),
+      notNull: true,
+      primaryKey: true,
+    },
+
+    name: {
+      type: "varchar(60)",
+      notNull: true,
+      unique: true,
+    },
+
+    cpf: {
+      type: "varchar(11)",
+      notNull: true,
+      unique: true,
+    },
+
+    email: {
+      type: "varchar(254)",
+      notNull: true,
+      unique: true,
+    },
+
+    password: {
+      type: "varchar(60)",
+      notNull: true,
+    },
+
+    features: {
+      type: "varchar[]",
+      notNull: true,
+      default: `{}`,
+    },
+
+    created_at: {
+      type: "timestamp with time zone",
+      notNull: true,
+      default: pgm.func("(now() at time zone 'utc')"),
+    },
+
+    updated_at: {
+      type: "timestamp with time zone",
+      notNull: true,
+      default: pgm.func("(now() at time zone 'utc')"),
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {};

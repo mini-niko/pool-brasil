257adaeb0a12daf0c82b89256936149e3ba08706
Miniko <xavierdeoliveiramauricio@gmail.com>
2025-01-14 22:58:51 -0300

    refactor: add `status()` function in `infra/database.js`

--- infra/database.js
+++ infra/database.js
@@ -38,9 +38,31 @@ function getSSL() {
   return process.env.NODE_ENV === "production";
 }
 
+async function status() {
+  const openConnectionsQuery = await database.query(
+    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    [process.env.POSTGRES_DB],
+  );
+  const openConnections = openConnectionsQuery.rows[0].count;
+
+  const maxConnectionsQuery = await database.query("SHOW max_connections;");
+  const maxConnections = maxConnectionsQuery.rows[0].max_connections;
+
+  const versionQuery = await database.query("SHOW server_version;");
+  const version = versionQuery.rows[0].server_version;
+
+  return {
+    status: "healthy",
+    open_connections: openConnections,
+    max_connections: parseInt(maxConnections),
+    version,
+  };
+}
+
 const database = {
   getNewClient,
   query,
+  status,
 };
 
 export default database;

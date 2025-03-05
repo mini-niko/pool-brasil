c0a90df9ad1702f5c531bc5ef4d2d92cd5f9dc4e
Miniko <xavierdeoliveiramauricio@gmail.com>
2025-03-05 17:31:49 -0300

    refactor: refactor interface

---
+++ pages/interface/components/Input.js
@@ -0,0 +1,32 @@
+import classNames from "classnames";
+import Box from "./Box";
+
+function Input({
+  children,
+  label,
+  name,
+  type = name,
+  value,
+  setValue,
+  placeholder = "",
+  className = "",
+}) {
+  return (
+    <Box id={`${name}-field-box`} flex={true} direction="col" gap={1}>
+      <label for={name} className="font-bold text-pool-dark text-xl md:text-lg">
+        {label}
+      </label>
+      <input
+        type={type}
+        name={name}
+        className={`focus:outline-none rounded-md drop-shadow-xl w-64 px-4 py-2 text-sm md:px-3 md:py-1 md:text-xs shadow-pool-light/25 text-pool-dark ${className}`}
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      {children}
+    </Box>
+  );
+}
+
+export default Input;

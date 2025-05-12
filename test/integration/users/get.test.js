b5d39963035530bda50a0b6e297e43283af71279
mini-niko <xavierdeoliveiramauricio@gmail.com>
2025-05-11 22:58:04 -0300

    test: fix `/api/v1/users` GET test not pass

--- test/integration/users/get.test.js
+++ test/integration/users/get.test.js
@@ -1,20 +1,25 @@
 import userValidate from "@/models/validation/user";
 import orchestrator from "test/orchestrator";
 
-let mockUser;
+let sessionToken;
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
   await orchestrator.cleanDatabase();
   await orchestrator.upMigrations();
-  mockUser = await orchestrator.createUser(["client"]);
+  const mockUser = await orchestrator.createUser(["admin"]);
+  sessionToken = await orchestrator.setSession(mockUser);
 });
 
 describe("GET to /api/v1/users", () => {
   describe("Annonymous User", () => {
     describe("Retrieving all users", () => {
       test("With valid ID", async () => {
-        const response = await fetch(`http://localhost:3000/api/v1/users`);
+        const response = await fetch(`http://localhost:3000/api/v1/users`, {
+          headers: {
+            Cookie: `sessionToken=${sessionToken}`,
+          },
+        });
 
         expect(response.status).toBe(200);
 

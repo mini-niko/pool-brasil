a6759faeea2622f6dd473f274e2f5474c4aac583
mini-niko <xavierdeoliveiramauricio@gmail.com>
2025-05-11 22:42:44 -0300

    test: modify tests for `/api/v1/migrations` endpoint

--- test/integration/migrations/post.test.js
+++ test/integration/migrations/post.test.js
@@ -1,44 +1,52 @@
 import orchestrator from "test/orchestrator";
 
+let loggedUser;
+let sessionToken;
+
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
   await orchestrator.cleanDatabase();
+  await orchestrator.upMigrations();
+  loggedUser = await orchestrator.createUser(["admin"]);
+  sessionToken = await orchestrator.setSession(loggedUser);
 });
 
 describe("POST to /api/v1/migrations", () => {
   describe("Annonymous user", () => {
-    describe("Running pending migrations", () => {
-      test("For the first time", async () => {
-        const response = await fetch(
-          "http://localhost:3000/api/v1/migrations",
-          {
-            method: "POST",
-          },
-        );
-
-        expect(response.status).toBe(201);
-
-        const body = await response.json();
-
-        expect(Array.isArray(body)).toBe(true);
-        expect(body.length).toBeGreaterThan(0);
+    test("Running pending migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "POST",
       });
 
-      test("For the second time", async () => {
-        const response = await fetch(
-          "http://localhost:3000/api/v1/migrations",
-          {
-            method: "POST",
-          },
-        );
+      expect(response.status).toBe(401);
 
-        expect(response.status).toBe(200);
+      const body = await response.json();
 
-        const body = await response.json();
-
-        expect(Array.isArray(body)).toBe(true);
-        expect(body.length).toBe(0);
+      expect(body.name).toBe("UnauthorizedError");
+      expect(body.message).toBe(
+        "O usuário não pode executar a operação requisitada.",
+      );
+      expect(body.action).toBe(
+        'Verifique se o usuário possui algumas das seguintes permissões: "admin".',
+      );
+      expect(body.status_code).toBe(401);
+    });
+  });
+  describe("Admin user", () => {
+    test("Running pending migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "POST",
+        headers: {
+          Cookie: `sessionToken=${sessionToken}`,
+        },
       });
+
+      expect(response.status).toBe(200);
+
+      const body = await response.json();
+
+      expect(Array.isArray(body)).toBe(true);
+      expect(body).toHaveLength(0);
     });
   });
 });
